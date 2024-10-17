f41970cb3b6f4c14de345782421225993608527f
JannikHoog <80885116+JannikHoog@users.noreply.github.com>
2024-10-17 09:05:43 +0200

    SBatchJob added as KaiTest.sh

---
+++ KaiTest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J FirstTestsWithCluster
+#SBATCH -o ./%x.%j.%N.out
+#SBATCH -D ./build
+#SBATCH --get-user-env
+#SBATCH --clusters=cm2
+//#SBATCH --partition=cm2
+//#SBATCH --mem=150MB
+#SBATCH --cpus-per-task=1
+#SBATCH --export=NONE
+#SBATCH --mail-user=jannik.hoog@gmx.de
+#SBATCH --mail-type=end
+#SBATCH --time=00:01:00
+
+./md-flexible --yaml-filename fallingDrop.yaml
